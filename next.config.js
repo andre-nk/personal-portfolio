0a7aa86af8915eee92638d6800b4686e77890362
Andreas Notokuusmo <andreasnotokusumo23@gmail.com>
2021-12-21 16:22:02 +0700

    :sparkles: Favicon, 404 Page, Full-Text Search Update, Filter Removal

--- next.config.js
+++ next.config.js
@@ -4,5 +4,9 @@ module.exports = {
     domains: [
       "images.ctfassets.net"
     ]
+  },
+  env: {
+    SPACE_ID: process.env.SPACE_ID,
+    ACCESS_TOKEN: process.env.ACCESS_TOKEN,
   }
 }
---
+++ pages/404.js
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
+import MainButton from "../components/ui/MainButton";
+
+export default function ErrorPage() {
+  return (
+    <div className="flex flex-col w-full justify-center items-center mb-12 px-8">
+      <div className="lg:w-3/12">
+        <Image src="/images/404.png" height={480} width={480} />
+      </div>
+      <div className="flex flex-col justify-center items-center space-y-4">
+        <h2 className="text-[1.65rem] font-semibold">Oops! The 404 Page!</h2>
+        <p className="text-[1rem] text-center leading-loose">
+          The thing you are looking for is not here. <br />
+          Better get to the safe place at{" "}
+          <code className="font-mono bg-hover-white rounded-md m-2 p-1.5">
+            "/"
+          </code>
+        </p>
+        <div className="pt-8 w-full">
+          <Link href="/" passHref>
+            <MainButton content={"Redirect me please!"} />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
