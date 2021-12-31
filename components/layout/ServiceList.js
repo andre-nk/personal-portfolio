ff18a9297aff22a366ee8633e86e2dd37588028f
Andreas Notokuusmo <andreasnotokusumo23@gmail.com>
2021-12-31 16:24:21 +0700

    :sparkles: Service Card Thumbnail Update

--- components/layout/ServiceList.js
+++ components/layout/ServiceList.js
@@ -9,12 +9,13 @@ export default function ServiceList(props) {
   return (
     <div className="flex flex-col space-y-8 md:grid md:grid-cols-2 md:gap-12 md:space-y-0 lg:grid-cols-4 pt-10">
       {serviceRes.map((service) => {
-        const { title, caption } = service.fields;
+        const { title, caption, thumbnail } = service.fields;
         return (
           <ServiceCard
             data-aos="fade-right"
             key={service.sys.id}
             title={title}
+            image={thumbnail.fields.file}
             caption={documentToReactComponents(caption)}
           />
         );
--- components/ui/ServiceCard.js
+++ components/ui/ServiceCard.js
@@ -1,14 +1,23 @@
 import Link from "next/link";
+import Image from "next/image";
 
-export default function ServiceCard({ slug, imageURL, title, caption }) {
+export default function ServiceCard({ image, title, caption }) {
   return (
     <Link href="/services" passHref>
       <div className="cursor-pointer h-auto w-full space-y-4 hover:bg-hover-white duration-200 rounded-lg">
-        {/* IMAGE URL HERE */}
-        <div className="w-full bg-gray-300 rounded-lg h-[11.25rem]" />
+        <div className="w-full bg-gray-300 rounded-lg h-[11.25rem] object-fill overflow-hidden">
+          <Image
+            src={"https:" + image.url}
+            alt="project_thumbnail"
+            width={image.details.image.width}
+            height={image.details.image.height}
+          />
+        </div>
         <div className="space-y-1.5 px-4 pb-5">
           <h2 className="text-[22px] font-semibold">{title}</h2>
-          <div className="text-[14px] text-primary-black opacity-70 prose">{caption}</div>
+          <div className="text-[14px] text-primary-black opacity-70 prose">
+            {caption}
+          </div>
         </div>
       </div>
     </Link>
